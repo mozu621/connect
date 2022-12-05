11f11ba8c56034fdeffa94cceb1bd8880151bfe5
Kazuya Mori <kazuya.mori0621@gmail.com>
2022-12-05 22:42:58 +0900

    sever add domain

--- api_connect/settings.py
+++ api_connect/settings.py
@@ -59,7 +59,10 @@ MIDDLEWARE = [
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
     'https://localhost:3000',
-    'https://vercel.com/mozu621/next-js-connect-a3bk'
+    'https://vercel.com/mozu621/next-js-connect-a3bk',
+    'https://connect-mozu.site'
+
+
 ]
 
 ROOT_URLCONF = 'api_connect.urls'

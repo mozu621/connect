f67395aad7e2b3b02cb5c9f88107ae5b79c00eec
Kazuya Mori <kazuya.mori0621@gmail.com>
2022-12-08 22:27:05 +0900

    change setting.py second

--- api_connect/settings.py
+++ api_connect/settings.py
@@ -55,13 +55,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# 追加
-CORS_ORIGIN_WHITELIST = [
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
     'http://localhost:3000',
     'https://localhost:3000',
     'https://vercel.com/mozu621/next-js-connect-a3bk',
     'https://connect-mozu.site',
 ]
+
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
